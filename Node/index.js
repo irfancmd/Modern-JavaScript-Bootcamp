b4cbe9bf2d14b85cbfb42e1a91f86b2aafed581b
Chowdhury Mohammad Irfanuddin <irfanuddincmd@gmail.com>
2023-03-10 02:50:59 +0600

    Looking at different arguments passed by nodejs

--- Node/index.js
+++ Node/index.js
@@ -1,3 +1,4 @@
-const message = require("./myScript.js");
-
-console.log(message);
+// When we run a javascript file in nodejs, it's contents are automatically
+// wrapped around a function that gives us access to some arguments. This funtion
+// is then executed by the system
+console.log(arguments);
